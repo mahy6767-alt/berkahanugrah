521c0a3688297ef5507913311c01bb414ec311b2
mahy6767-alt <mahy6767@gmail.com>
2025-12-18 16:33:43 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="lyzspgi0wzbtmzn6sg30tnmvgy6bqy" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
